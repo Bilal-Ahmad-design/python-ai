154295d608aced6a8a490d64a6e765bca276677a
Bilal <optimist1356@gmail.com>
2026-01-01 17:37:35 +0500

    Tuple added

--- main.py
+++ main.py
@@ -2,19 +2,45 @@ import pyttsx3
 
 
 #  Program initiation
-engine = pyttsx3.init()
+
+# sales = 3000
+# expenses = 4500
+
+# total_revenue = sales - expenses
+# other_income = 1000
+
+# result = other_income <= total_revenue
+
+# print (result)
+
+# Find_Type = type (result)
+
+# print (type (sales))
+# type_casting = float(sales)
+
+# print (type (sales))
+
+# type_sales = type(type_casting)
+# print (type_casting)
+
+# # jaunary = input ("APIs build this month around the globe: ") 
+# # febuary = input ("APIs build in Febuary around the globe: ")
+# dummy_test = "This is going to be my dymmy test for functions"
+
+# print ("""
+# The number of APIs build are: 
+# The number of APIs build in January are: 
+# """,
+# jaunary, febuary )
+# """ in_nutshell = print ("In nutshell, the amount of APIs build in the first two months of the year 2025 is: ", int(jaunary) + int(febuary), "and this is done with slicing the january and removing the 'APIs' from the start of the sentence. ")
+# """
+
+# print (jaunary[0 : 2])
+# print (dummy_test[4: ])
 
 
-rate = engine.getProperty('rate')
 
-engine.setProperty('rate', 200)  # setting up new voice rate
+s = ['Bilal', 1356, 10.56, False, 1, '2026']
+t = ('Tuple', 1, 2025, False)
 
-# pyttsx3.speak("At the speed of 200")
-print (rate)
-
-
-volume = engine.getProperty('volume')
-print (volume)
-
-engine.setProperty('volume', 0.8)
-
+print(type(t))
